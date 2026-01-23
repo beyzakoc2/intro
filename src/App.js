2f60325fbbc6fe5f7bed006b075b3c11510e1f37
beyzakoc2 <beyzab2@icloud.com>
2026-01-23 13:42:45 +0300

    I learned page segmentation and definitions.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import react from "react";
+import Navi from "./Navi";
+import CategoryList from "./CategoryList";
+import ProductList from "./ProductList";
+import { Container, Row, Col } from "reactstrap";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container>
+        <Row>
+          <Navi></Navi>
+        </Row>
+        <Row>
+          <Col xs="3">
+            <CategoryList></CategoryList>
+          </Col>
+          <Col xs="9">
+            <ProductList></ProductList>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
