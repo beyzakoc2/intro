af49070eaa1d11e579556e290a029eeb0f880d0a
beyzakoc2 <beyzab2@icloud.com>
2026-01-28 14:15:22 +0300

    added the products and added the filtering

--- src/App.js
+++ src/App.js
@@ -1,31 +1,64 @@
-import react from "react";
+import React, { Component } from "react";
 import Navi from "./Navi";
 import CategoryList from "./CategoryList";
 import ProductList from "./ProductList";
 import { Container, Row, Col } from "reactstrap";
-function App() {
-  let productInfo = { title: "Product List" };
-  let categoryInfo = { title: "Category List" };
-  // let titleProduct = "Product List";
-  // let titleCategory = "Category List"; title={titleCategory} şeklinde yazılır
-  // ister tanımlayarak ister direkt "title=Category List" yazarak okunur hale getirebiliriz.
-  return (
-    <div>
-      <Container>
-        <Row>
-          <Navi></Navi>
-        </Row>
-        <Row>
-          <Col xs="3">
-            <CategoryList info={categoryInfo}></CategoryList>
-          </Col>
-          <Col xs="9">
-            <ProductList info={productInfo}></ProductList>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+
+export default class App extends Component {
+  state = { currentCategory: "", products: [] };
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  changeCategory = (category) => {
+    this.setState({
+      currentCategory: category.categoryName,
+    });
+    this.getProducts(category.id);
+  };
+
+  getProducts = (categoryId) => {
+    let url = "http://localhost:3000/products";
+    if (categoryId) {
+      url += "?categoryId=" + categoryId;
+    }
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => this.setState({ products: data }));
+  };
+
+  render() {
+    let productInfo = { title: "Product List" };
+    let categoryInfo = { title: "Category List" };
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Navi></Navi>
+          </Row>
+          <Row>
+            <Col xs="3">
+              <CategoryList
+                currentCategory={this.state.currentCategory}
+                changeCategory={this.changeCategory}
+                info={categoryInfo}
+              ></CategoryList>
+            </Col>
+            <Col xs="9">
+              <ProductList
+                products={this.state.products}
+                currentCategory={this.state.currentCategory}
+                info={productInfo}
+              ></ProductList>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
 
-export default App;
+// let titleProduct = "Product List";
+// let titleCategory = "Category List"; title={titleCategory} şeklinde yazılır
+// ister tanımlayarak ister direkt "title=Category List" yazarak okunur hale getirebiliriz.
